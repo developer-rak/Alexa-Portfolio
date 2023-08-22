013b4a0d0e03af50762ee0e82e80cbb600406c23
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-08-22 17:16:21 +0500

    js for services cards

--- script.js
+++ script.js
@@ -64,3 +64,26 @@ tabs.forEach(tab => {
       tab.classList.add('qualification_active')
    })
 })
+
+/*==SERVICES MODAL==*/
+const modalViews = document.querySelectorAll('.services_modal'),
+   modalBtn = document.querySelectorAll('.services_button'),
+   modalCloses = document.querySelectorAll('.services_modal-close')
+
+let modal = function (modalClick) {
+   modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtn.forEach((modalBtn, i) => {
+   modalBtn.addEventListener('click', () => {
+      modal(i)
+   })
+})
+
+modalCloses.forEach((modalClose) => {
+   modalClose.addEventListener('click', () => {
+      modalViews.forEach((modalView) => {
+         modalView.classList.remove('active-modal')
+      })
+   })
+})
