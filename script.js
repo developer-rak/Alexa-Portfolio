a3ccbf16e12068dbc335f45d2f1bb11d4e9d02b6
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-08-25 16:55:21 +0500

    swiper

--- script.js
+++ script.js
@@ -89,4 +89,16 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==PORTFOLIO SWIPER==*/
-let swiper = new Swiper('.seiper-container')
+let swiper = new Swiper(".portfolio_container", {
+   cssMode: true,
+   loop: true,
+
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+});
